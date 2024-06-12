94d6a17bf68e5d97cb07e0028c85797865145237
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-06-12 19:04:19 -0400

    Fixed main

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from maze_solver import Maze
 if __name__ == "__main__":
     maze = Maze()
     
-    # The visualizations of each algorithm
-    maze.animate_backtrack()
-    maze.animate_bfs()
+    print("1. BFS (No visualization)\n2. Backtracking (No visualization)\n 3. BFS (Visualization)\n4. Backtracking (Visualization)")
+    user_choice = input("Enter what algorithm you want to view (1, 2, 3, or 4) or any other key to exit:")
     
-    # The paths printed via a list
-    backtracking_path = maze.backtrack()[0]
-    bfs_paths = maze.bfs()[0]
-    print(backtracking_path)
-    print(bfs_paths)
+    if user_choice == "1":
+        print(maze.bfs()[0])
+    elif user_choice == "2":
+        print(maze.backtrack()[0])
+    elif user_choice == "3":
+        maze.animate_bfs()
+    elif user_choice == "4":
+        maze.animate_backtrack()
+    else:
+        print("Bye bye!")
+    
+    
     
