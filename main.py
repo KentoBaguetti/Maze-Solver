99b3e00f6715b6aeba47e1258dc35b6d575238aa
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-06-11 22:18:40 -0400

    Main

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from maze_solver import Maze
 if __name__ == "__main__":
     maze = Maze()
     
+    # The visualizations of each algorithm
+    maze.animate_backtrack()
+    maze.animate_bfs()
+    
+    # The paths printed via a list
+    backtracking_path = maze.backtrack()[0]
+    bfs_paths = maze.bfs()[0]
+    print(backtracking_path)
+    print(bfs_paths)
+    
