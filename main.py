8d2da642691111e850609e208336ce76d4e19b74
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-07-26 21:38:34 -0400

    add parenthesis to backtracking func

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from maze_solver import Maze
 
 if __name__ == "__main__":
     maze = Maze()
-    
+
     print("1. BFS\n2.Backtracking\n3. Dijkstras Algorihtm")
-    user_choice = input("Enter what algorithm you want to view (1, 2, or 3) or any other key to exit:")
-    
+    user_choice = input(
+        "Enter what algorithm you want to view (1, 2, or 3) or any other key to exit: "
+    )
+
     if user_choice == "1":
-       maze.animate_bfs()
+        maze.animate_bfs()
     elif user_choice == "2":
-        maze.animate_backtrack
+        maze.animate_backtrack()
     elif user_choice == "3":
         maze.animate_dijkstra()
     else:
         print("Bye bye!")
-    
-    
-    
